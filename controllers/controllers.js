e8261a8e62e3e6ace17bc3b10774b09364bfd20c
AndriyPapirnyk <papirnykandriy@gmail.com>
2024-01-27 14:36:46 +0200

    creating test GET method

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 //
 exports.test = async (req, res) => {
-  res.send('it is server')
+  res.send(req.deviceId)
 }
 
 // exports.verifyUser = async (req, res) => {
